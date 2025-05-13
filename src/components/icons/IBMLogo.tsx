5249b3e1f5a0cbe686550311e52f792d86e8f50b
YASHU YADAV A <100356083+Yashu9353@users.noreply.github.com>
2025-05-13 07:13:43 +0000

    Your meaningful commit message here

--- src/components/icons/IBMLogo.tsx
+++ src/components/icons/IBMLogo.tsx
@@ -1,15 +1,11 @@
-
 const IBMLogo = ({ className = "" }: { className?: string }) => {
   return (
-    <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 32 32"
+    <img
+      src="/images/item_horizontal_with_1852881195.jpeg"
+      alt="IBM Logo"
       className={className}
-      fill="currentColor"
       aria-label="IBM"
-    >
-      <path d="M0 0v32h32V0zm2 2h28v28H2zm9.5 5v1h9v-1zm0 2v1h9v-1zm-6 2v1h21v-1zm0 2v1h21v-1zm0 2v1h21v-1zm6 2v1h9v-1zm0 2v1h9v-1zm-6 2v1h21v-1zm0 2v1h21v-1zm0 2v1h21v-1z" />
-    </svg>
+    />
   );
 };
 
